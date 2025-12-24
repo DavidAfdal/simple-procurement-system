b7deb3e2e0a1f6dd0d21dcdbcfa41b032d558ebf
DavidAfdal <davidafdal7@gmail.com>
2025-12-24 14:42:26 +0800

    feat: menmabahkan fungsi build router public dan private

--- backend/internal/http/router/router.go
+++ backend/internal/http/router/router.go
@@ -5,9 +5,21 @@ import (
 	"github.com/DavidAfdal/purchasing-systeam/pkg/route"
 )
 
+const (
+	Admin = "Admin"
+	Staff = "Staff"
+)
+
+var (
+	allRoles  = []string{Admin, Staff}
+	onlyAdmin = []string{Admin}
+	onlyStaff = []string{Staff}
+)
+
 func AppPublicRoute(handler *handler.Handler) []*route.Route {
 	userHandler := handler.UserHandler
 	supplierHandler := handler.SupplierHandler
+	itemHandler := handler.ItemHandler
 
 	return []*route.Route{
 		{
@@ -30,20 +42,85 @@ func AppPublicRoute(handler *handler.Handler) []*route.Route {
 			Path:    "/suppliers/:id",
 			Handler: supplierHandler.GetSupplier,
 		},
+		{
+			Method:  "GET",
+			Path:    "/items",
+			Handler: itemHandler.GetItems,
+		},
+		{
+			Method:  "GET",
+			Path:    "/items/:id",
+			Handler: itemHandler.GetItemByID,
+		},
 	}
 }
 
 func AppPrivateRoute(handler *handler.Handler) []*route.Route {
-
-	// userHandler := handler.UserHandler
-	// supplierHandler := handler.SupplierHandler
+	userHandler := handler.UserHandler
+	supplierHandler := handler.SupplierHandler
+	itemHandler := handler.ItemHandler
 	purchasingHandler := handler.PurchasingHandler
 
 	return []*route.Route{
 		{
 			Method:  "POST",
+			Path:    "/users/logout",
+			Handler: userHandler.Logout,
+			Roles:   allRoles,
+		},
+		{
+			Method:  "POST",
+			Path:    "/suppliers",
+			Handler: supplierHandler.CreateSupplier,
+			Roles:   allRoles,
+		},
+		{
+			Method:  "PUT",
+			Path:    "/suppliers/:id",
+			Handler: supplierHandler.UpdateSupplier,
+			Roles:   allRoles,
+		},
+		{
+			Method:  "DELETE",
+			Path:    "/suppliers/:id",
+			Handler: supplierHandler.DeleteSupplier,
+			Roles:   allRoles,
+		},
+		{
+			Method:  "POST",
+			Path:    "/items",
+			Handler: itemHandler.CreateItem,
+			Roles:   allRoles,
+		},
+		{
+			Method:  "PUT",
+			Path:    "/items/:id",
+			Handler: itemHandler.UpdateItem,
+			Roles:   allRoles,
+		},
+		{
+			Method:  "DELETE",
+			Path:    "/items/:id",
+			Handler: itemHandler.DeleteItem,
+			Roles:   allRoles,
+		},
+		{
+			Method:  "POST",
 			Path:    "/purchasings",
 			Handler: purchasingHandler.CreatePurchasing,
+			Roles:   allRoles,
+		},
+		{
+			Method:  "GET",
+			Path:    "/purchasings",
+			Handler: purchasingHandler.GetPurchasing,
+			Roles:   allRoles,
+		},
+		{
+			Method:  "GET",
+			Path:    "/purchasings/me ",
+			Handler: purchasingHandler.GetMyPurchasings,
+			Roles:   allRoles,
 		},
 	}
 }
